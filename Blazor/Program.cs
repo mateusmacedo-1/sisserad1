044ee25c41ab33376425001c1c5939de445fb3d5
mateus wesley <mateuswesley314@gmail.com>
2024-03-16 13:25:13 -0300

    tirando codigo desnecessario

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -10,18 +10,5 @@ builder.Services.AddHttpClient("ApiSerad", client =>
 {
     client.BaseAddress = new Uri("https://sisserad-api.onrender.com");
 });
-builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-var app = builder.Build();
-
-app.Use(async (context, next) =>
-{
-    await next();
-
-    if (context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
-    {
-        context.Request.Path = "/index.html";
-        await next();
-    }
-});
 await builder.Build().RunAsync();
