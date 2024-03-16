da6f0615a2dc256b990958f0ff4624c9d262a15d
mateus wesley <mateuswesley314@gmail.com>
2024-03-16 15:07:39 -0300

    atualizando program.cs e o csproj

--- ClientBlazor/Program.cs
+++ ClientBlazor/Program.cs
@@ -6,6 +6,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddHttpClient("ApiSerad", client =>
+{
+    client.BaseAddress = new Uri("https://sisserad-api.onrender.com");
+});
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
