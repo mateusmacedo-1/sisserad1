daef1d79db387e3f6171c56bb54b5c29a1d3efb6
mateus wesley <mateuswesley314@gmail.com>
2024-03-20 11:56:10 -0300

    feat(mudblazor)
    1. Adicionado propriedade documento formatado para exibir na tabela
    2. Trocando os campos de cadastro pelos da mudblazor
    3. Trocando tabela pela da mudblazor

--- Application/ViewModels/ClienteViewModel.cs
+++ Application/ViewModels/ClienteViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Enums;
+﻿using System.Text.Json.Serialization;
+using Domain.Enums;
 
 namespace Application.ViewModels;
 
@@ -10,6 +11,54 @@ public class ClienteViewModel
     public string? Email { get; set; }
     public string? Telefone { get;  set; }
     public ICollection<EnderecoViewModel> Enderecos { get; set; } = [];
-    public string? Documento { get; set; }
-    public string Tipo { get; set; }
+
+
+    private string? documento;
+    public string? Documento 
+    {
+        get{ return documento; }
+        set {
+            documento = value;
+            FormatarDocumento();
+        } 
+    }
+    public string? DocumentoFormatado { get; set; }
+    
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public TipoCliente Tipo { get; set; }
+
+    public bool SearchString(string? searchString){
+        if (string.IsNullOrWhiteSpace(searchString))
+            return true;
+        if (Nome.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            return true;
+        if (Representante.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            return true;
+        if ($"{Telefone} {Email} {Documento}".Contains(searchString))
+            return true;
+        return false;
+    }
+
+
+    private void FormatarDocumento()
+    {
+        if (Tipo == TipoCliente.Física)
+        {
+            DocumentoFormatado = FormatarCPF(Documento);
+        }
+
+        else
+        {
+            DocumentoFormatado = FormatarCNPJ(Documento);
+        }
+    }
+    static string FormatarCPF(string cpf)
+    {
+        return Convert.ToUInt64(cpf).ToString(@"000\.000\.000\-00");
+    }
+
+    static string FormatarCNPJ(string cnpj)
+    {
+        return Convert.ToUInt64(cnpj).ToString(@"00\.000\.000\/0000\-00");
+    }
 }
